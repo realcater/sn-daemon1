807d1d70de2cbf91e39eb850b1d0ccddade28621
Dmitry Dementyev <dmitryd@dmitrys-mbp.dlink>
2019-08-31 13:41:09 +0300

    Add env vars

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -17,11 +17,18 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let databasePort = Int(Environment.get("DATABASE_PORT") ?? "") ?? 5432
+    
     let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: "localhost",
-        username: "vapor",
-        database: "vapor",
-        password: "password")
+        hostname: hostname,
+        port: databasePort,
+        username: username,
+        database: databaseName,
+        password: password)
     let database = PostgreSQLDatabase(config: databaseConfig)
 
     var databases = DatabasesConfig()
